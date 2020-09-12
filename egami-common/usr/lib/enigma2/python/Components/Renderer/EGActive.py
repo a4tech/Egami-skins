dc29bcecb07c36227a9657fd801869b35db61437
a4tech <darezik@gmail.com>
2020-09-12 08:11:11 +0200

    Update skin

---
+++ egami-common/usr/lib/enigma2/python/Components/Renderer/EGActive.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+import zlib, base64
+exec(zlib.decompress(base64.b64decode('eJxtjsEKgzAMhu99ity0OAYt4mGsTyIbqI1acK2klW1vv9ht7LJLwp/vz5+42xooQd9FbGphcQScBnquqYzqELU8CYjKvPGxb2qLQ7DIUDLQf4BmcJ/dgrCg3+U5d7Un8UZlohYQtmSKQsAYCJy3+OAK1PkJy48729lWmWGmMpDlYZutF3nNUn8lHyRMG3n4fYM+f8MBUrwAQDRGzA==')))
