5ab8f13158eeb45e8af5faaf39f07d4ae03a1302
madhouse <35741027+m4dhouse@users.noreply.github.com>
2021-07-08 01:07:49 +0200

    Update EGPoster

--- egami-common/usr/lib/enigma2/python/Components/Renderer/EGPoster.py
+++ egami-common/usr/lib/enigma2/python/Components/Renderer/EGPoster.py
@@ -1,3 +1,190 @@
-#!/usr/bin/env python
-import zlib, base64
-exec(zlib.decompress(base64.b64decode('eJw9V0tzpLiW3tev6ImJmOoO91wjyLSTRS8kEkmJbaokUg+0IxEPF4LENmWwf/1Vze3pnYI8kNI530v//V+3P99eby/P020zvf82fyz9dfryPM7X1+W3T/d8+fNSvTV3uy/t63X87T/r3/7++XK3a6b6aps//co2v1b/KUsJrJfn9+b/C5uufv2Yly/VPP/1FYDgcP8/YQTCw9cvzdbUv//z9u9/F/7+VaZOqi2utZvPFG9CuFhywDVLT5VyKJFsd0wwxEgaK9xeaH5AMnInQ+IbLsonmOKOwpI2fK+k3II0WEoN3jIRLNVZm6N2vIL4Cm3AHtRU01bun6vQoFLjCgczktK+mGh/JwKWJSs3BbVDq/LGKqSgrinEmUEMdQXpTRPOtcRACLXP2sm9lsCcUDi/X6RAXNqdcPnVTCi2GDcozDutlmMDss2m5l4EbpVk5qXLSwUCdAyzpKAzpgMKObQPil6hb8BrSdAsJB+LtJfNxokhbw91VKYKuAWP3UOlMEqYC9sUrFUojQniSwPKXKX7UTi5atlju7qPKoVYMXFuA3vDXLah0BwNGBq91pmKeEJTeXPRTyc45f0Z4Bmpa1YT/JwwfNCpIFQbmtDgAQZvpApSmIS5qYbrNzlYwtL+XUWxrddSauAGSPa7Copzg0zFIN9fJv7a/BrK+lSXbg61jFUa9QWKXK0kQArIgxFDotwC6ylTEMdGbw7ZNO8bxUkyZksygYG7K2rx8gp71PHOwmKEqHL8+QL6A8Tu3KyZk5M7qDA/GJTvWhffIsEfoczxmcl7CXKWanvUCNzIjnkM5O+U5QEaM1dH7taEGPCU+77xyqrtjrk8qCfwoDwmtMDvJkSXCu8nSMx3NriGieXYRnLSwD4X6poaKZXadhc2gMIOs5ayJDpg+rjFq1nRg1SLLZ38qMT1sRntK1QZPoN5oAyfhYYNi5YDJflrO9a4Vpkq1yu2JIcWmpW6uCzCsoUCle2WfzY87iHHmxprWOGts133rWCihsP8zLvsdPFPILmiGm6nVshKDHyrhvx8AejRY/14Bj2CA3otlE3FAHJL+Ksm22xHHihPLERlcoQmLChIL6GtitToerSHVsWJBvu1ZPFVyiwWabyi4I2mYGthB3ZVN8/twCsEDkSj/gRXpKvpBGkfx5LGWI4IJtMhk1EM9MTKhs3f4XQ42WF/kAohPcaTZCaUU/yiO9tXoD7pMKglyzxnUki6XpVDmhGJqIBBU4/uBVFe1zB3FeO/+MUq3Vul3iCaNsR8iwh7wkpupgSuUlHeVpO7OWJzqCe3VCJDunf3bMo/qqg/NTTAbBqgUdmj8Xzm9GBVl90ncB9XBLWtfjoL0KvEc0axt0auy6WQsjlL8ahFesTRvjddBy+BfLRh35ZhncoOvwmJUgveTqU6aZperRcaehltb6Xx+mTrFmZ3chxOLWWsdTxoO/BZ4iyy6ZLCrr/zPCzE2Cu6Yi5VzjQ+ZTXm3yuANdL5XnsqiOAtMR1M1drLhOzfC9kRsWY/pY5nr5FMyIy0EztK5jakdyQBB889SFqCLgIfHmq/h2bl53MUM7VtgcJbbDrb8jBmXs+2Ap8q399UT+bRdPMFkeWlkRhbCc5EgkcUpA98zI7QbXWCbcHJHhuCuA2eMjHIjxbPk5Qn6vWmgWl+aSeLLJYHEXTQsDdyiboEkuUJgSfYDKUoOokLXFNJ0M8S5zGaxGNCn3CVLjidzAFF8MRcnFbKhjVG9zWeD8oZ07rTUWhkfedXlfbTGS8v7Woij/F9tRqqpVVm2CIR8vbMzWcBlr1e+7mVaCgCszdgjxTJf1yC/NhQ5LV9Fkpmryi4Hv2ZH87M4Qb7na/4VuB93wJ+d/aSyQYjDDYnG4FD6baXku8/fc1zg9GYhksFwysy0yk9puBBBjXVDgA+zUUd2LvW637r9lhI2xedLaXESsBMnmnA+bSfLcAd3Ja5xfaHxTO5YGfKCVKheNfowwP3PnmZ9qdiRbbxByzBwo8gCyQwrh399932WUNeaJgXrcCRECaqRtR6A7vxe301nG/t4MpGL0jIt5SDpUgE/gkj43EKNXOsglOMypA9tGTfa4h3x8ijg6wIRx57JK/ZIFOZZp/FFF/qFZdQ8bweLGom++k16L4U/Y2OcC+1valpX8o03szKiaLys/Ea2ozypZj6+2MqT1q/UQxky8e+Eh2uGwW+CW3SMpLPyrEMjvHZArPJaDmV4XZMicAIg0c+sONRIO8TSCDwf31dahXfq07QYjqhgiyrHDLhffCd0YUneBvqgL9Wkwd3ZL+3ffxaOvRZaxm0NA9KIMtKofuSZKB03GeGXdasy2fZ8c8ytA3H/WYjG5TOhRcmZy4XUw5mz0TW2mneV3TZNZH52Yr+2tBsfwbuDnXIed0B3M1dQi0oZKzVNDPk0GiIrfUAduW4BIxI0QxzgCefN6Llp+fQuVqtqqI9kGzfmGm+oYQLQznQUbYWdPnmt3DXROxU4hkIim4aMrtyWm75KHObAtzI7F4Dv8cJdF5ECASo13g+NtEsmOSnGs6dovlLqeXV+5MUItuVAfBzN0kJ8GY2n5emLRNge7YC7AzYbpSOk5pwULlsgMDjVpgPk2JkQ3coaVZ4Dwf1un3oCd/xLb4lNDYt7kPOrcLr0sINHBRjKFH2VrOZIGyA1/JnSDLhv3nX4jKTFNkzm/d6kMqKNIFMXnUUtwIuVwji6DK42AzZg5RLJn1fzt4TW2leRIB2moqMTZls6Ob1hm9aHmCr3xKP2VuI53OpZ6fWfIMkv0mwTKxeJgXdzv9/DUGdmq73PdqniIAz/6XVg+ykZO1ZgccEyDsivBeG9YlQH2rRfC+GXhi4vdvgDVYSN2SwY0nnvokAaiXuLO51KdC+1WtyHPAdY3FZM25htH8q1poUAT5r5fU1WhPo3ANTT8SH3cg6ECsfio2yiYiQLmWZCZYWfGWodDWh/XLPkcQorUkK7KqpRZc0s1DBRzrloqDmxr91QjL+QbqcC2KvJkKfR4CCRKObSs6RFPLWz60oJ76VVNB2zA6WcnMcs1cVZezY9SEB8tfZHvkkP1sZew3MMVrxUWvwUYcIsNQn2IklhcdJy/M99x5oxzckoj6zIf/OqJdstoXai+oxgsSy3BYDPjYdshcIazngFzbNL8eRI6L3CZRZgnS8nbl4ZAqrBDwhw+aUEa/1KvP1xqopPpW9z4HhG0HAKdg5Wvp26dE90hD7NskZhdbjNJ+ZQzSJtmtFt9FrckZpmtpwmaDsaCLmG+kCVEfDU5vK8qxR4ZOixzoKGlYjOHKfOvdWhPhcptcj665pPfR+hhYf5QyMQCuR2a0G5tamPbQ+uBVB9kOJ2fg6n4E26WehdYduKN8OEuCyCYfMx2zayC2EdJmq7oqY3JHSX3o4n9PzaBufm5sSg5PqzAdJ46sGNbVTLmm4dxKaiUV8Tkh+kqD/XmFrC+B5qt2niOyKACcN2LMELikeOfee2TWRnW0qMV/lxEaP02j7oF5vhOx1G9VphcFOrrPUoXnw/J4Uy3oUwow7aekgv/nMHkCaP8gJXVqfoc5D9oN1slDC+px2OnMq/Vc2r1srMqkEOsxUsXLrOfheRXOsAXjjK34QwGeL4UQax4+c9G9NmEtGy1SMT5mWEDLBLyK0M4zqYx3Knuj5sdXxs78bLGUq7wxFtxXaZXwKSkayKIFzbGSfMg2E17oUeczbNb9AjwGhzGMb8ANjbkz1NqvQ3BodHytSNuUqd4Z7nnbZ1m48VRP6vNDTiYcz8979A/q5FXRH0hCgxGWOql1agDozvbkmbtu12/xYYGwuaMA+f3elW7ME5KBJ/bnGOSiheb1ET5Ar9GKl+1F4Xa4hZjKcVxT2gfXYKVx2X0r04T2lI+TXnBwo9fqEt7Q5rn/99fXPfy7ev/sb9b/+Xn79ubT/e/j6xx//+vsy/c+DP/748m8L1y+a')))
+# -*- coding: utf-8 -*-
+from Components.Renderer.Renderer import Renderer
+from enigma import iPlayableServicePtr, eServiceReference
+from enigma import ePixmap, ePicLoad, eTimer
+try:
+    from urllib.request import urlopen, quote
+except ImportError:
+    from urllib2 import urlopen, quote
+import json, os, re, socket
+from Components.AVSwitch import AVSwitch
+from Components.Pixmap import Pixmap
+from Tools.Directories import fileExists
+
+folder_path = "/tmp/MMPoster/"
+
+api_key = 'f23c2bd91113daf777dcba03990aea77'
+leng = open('/tmp/language.txt', 'r').readline().replace('config.osd.language=', '').replace('_', '-').replace('\n', '')
+
+class EGPoster(Renderer):
+    def __init__(self):
+        Renderer.__init__(self)
+        self.timer10 = eTimer()
+
+    def intCheck(self):
+        try:
+            socket.setdefaulttimeout(0.5)
+            socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(("8.8.8.8", 53))
+            return True
+        except:
+            return False
+
+    GUI_WIDGET = ePixmap
+    def changed(self, what):
+        try:
+            refer = self.source.serviceref.toString()
+            if refer.startswith('4097'):
+                if what[0] == self.CHANGED_CLEAR:
+                    self.instance.hide()
+                if what[0] != self.CHANGED_CLEAR:
+                    self.delay()
+            else:
+                if refer.startswith('5002'):
+                    if what[0] == self.CHANGED_CLEAR:
+                        self.instance.hide()
+                    if what[0] != self.CHANGED_CLEAR:
+                        self.delay()
+                else:
+                    pass
+        except:
+            pass
+
+    def infos(self):
+        if self.downloading:
+            return
+        self.downloading = True
+        service = self.source.service
+        info = None
+        if isinstance(service, iPlayableServicePtr):
+            info = service and service.info()
+            service = None
+        if not info:
+            return ''
+        name = service and info.getName(service)
+        if name is None:
+            name = info.getName()
+        name = name.replace('\xc2\x86', '').replace('\xc2\x87', '')
+        rep_name = ["(", ")", "[", "]", "u-", "3d", "-", "'", " S01 ", " S02 ", " S03 ", " S04 ", " S05 ", " S06 ", " S07 ", " S08 ", " S09 ", " S10 ", " S11 ", " S12 ", " S13 ", " S14 ",
+                     " S15 ", " S16 ", "E01", "E02", "E03", "E04", "E05", "E06", "E07", "E08", "E09", "E10", "E11", "E12", "E13", "E14",  "E15", "E16", "E17", "E18", "E19", "E20", "E21", "E22", "E23", "E24", "E25", ]
+        for j in range (1900, 2025):
+            rep_name.append(str(j))
+
+        for i in rep_name:
+            name = name.replace(i, '')
+        if service or info:
+            evntN = re.sub("([\(\[]).*?([\)\]])|(: odc.\d+)|(\d+: odc.\d+)|(\d+ odc.\d+)|(:)|( -(.*?).*)|(,)", "", str(name))
+            self.evntNm = evntN
+            self.pstrNm = "/tmp/MMPoster/{}.jpg".format(self.evntNm)
+            if self.intCheck():
+                try:
+                    self.instance.hide()
+                    url = 'http://api.themoviedb.org/3/search/tv?api_key={}&query={}'.format(api_key,quote(self.evntNm))
+                    url2 = urlopen(url).read().decode('utf-8')
+                    jurl = json.loads(url2)
+                    if 'results' in jurl:
+                        if 'id' in jurl['results'][0]:
+                            ids = jurl['results'][0]['id']
+                    url_2 = 'http://api.themoviedb.org/3/tv/' + str(ids) + '?api_key={}&language={}'.format(api_key,str(leng))
+                    url_3 = urlopen(url_2).read().decode('utf-8')
+                    data2 = json.loads(url_3)
+                    poster = data2['poster_path']
+                    if poster:
+                        self.url_poster = "http://image.tmdb.org/t/p/w185{}".format(poster)
+                        self.savePoster()
+                    self.timer10.stop()
+                except:
+                    try:
+                        self.instance.hide()
+                        url = 'http://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key,quote(self.evntNm))
+                        url2 = urlopen(url).read().decode('utf-8')
+                        jurl = json.loads(url2)
+                        if 'results' in jurl:
+                            if 'id' in jurl['results'][0]:
+                                ids = jurl['results'][0]['id']
+                        url_2 = 'http://api.themoviedb.org/3/movie/' + str(ids) + '?api_key={}&language={}'.format(api_key,str(leng))
+                        url_3 = urlopen(url_2).read().decode('utf-8')
+                        data2 = json.loads(url_3)
+                        poster = data2['poster_path']
+                        if poster:
+                            self.url_poster = "http://image.tmdb.org/t/p/w185{}".format(poster)
+                            self.savePoster()
+                        self.timer10.stop()
+                    except:
+                        self.timer10.stop()
+                        pass
+            else:
+                return
+
+    def delay(self):
+        self.downloading = False
+        try:
+            self.timer10.callback.append(self.infos)
+        except:
+            self.timer10_conn = self.timer10.timeout.connect(self.infos)
+        self.timer10.start(450, False)
+
+    def showPoster(self):
+        service = self.source.service
+        info = None
+        if isinstance(service, iPlayableServicePtr):
+            info = service and service.info()
+            service = None
+        if not info:
+            return ''
+        name = service and info.getName(service)
+        if name is None:
+            name = info.getName()
+        name = name.replace('\xc2\x86', '').replace('\xc2\x87', '')
+        rep_name = ["sd", "hd", "fhd", "uhd", "4k", "vod", "1080p", "720p", "blueray", "x264", "aac", "ozlem", "hindi", "hdrip", "(cache)", "(kids)", "[3d-en]", "[iran-dubbed]", "imdb", "top250", "multi-audio",
+                     "multi-subs", "multi-sub", "[audio-pt]", "[nordic-subbed]", "[nordic-subbeb]",
+
+                     "ae:", "al:", "ar:", "at:", "ba:", "be:", "bg:", "br:", "cg:", "ch:", "cz:", "da:", "de:", "dk:", "ee:", "en:", "es:", "ex-yu:", "fi:", "fr:", "gr:", "hr:", "hu:", "in:", "ir:", "it:", "lt:", "mk:",
+                     "mx:", "nl:", "no:", "pl:", "pt:", "ro:", "rs:", "ru:", "se:", "si:", "sk:", "tr:", "uk:", "us:", "yu:",
+
+                     "[ae]", "[al]", "[ar]", "[at]", "[ba]", "[be]", "[bg]", "[br]", "[cg]", "[ch]", "[cz]", "[da]", "[de]", "[dk]", "[ee]", "[en]", "[es]", "[ex-yu]", "[fi]", "[fr]", "[gr]", "[hr]", "[hu]", "[in]", "[ir]", "[it]", "[lt]", "[mk]",
+                     "[mx]", "[nl]", "[no]", "[pl]", "[pt]", "[ro]", "[rs]", "[ru]", "[se]", "[si]", "[sk]", "[tr]", "[uk]", "[us]", "[yu]",
+
+                     "-ae-", "-al-", "-ar-", "-at-", "-ba-", "-be-", "-bg-", "-br-", "-cg-", "-ch-", "-cz-", "-da-", "-de-", "-dk-", "-ee-", "-en-", "-es-", "-ex-yu-", "-fi-", "-fr-", "-gr-", "-hr-", "-hu-", "-in-", "-ir-", "-it-", "-lt-", "-mk-",
+                     "-mx-", "-nl-", "-no-", "-pl-", "-pt-", "-ro-", "-rs-", "-ru-", "-se-", "-si-", "-sk-", "-tr-", "-uk-", "-us-", "-yu-",
+
+                     "|ae|", "|al|", "|ar|", "|at|", "|ba|", "|be|", "|bg|", "|br|", "|cg|", "|ch|", "|cz|", "|da|", "|de|", "|dk|", "|ee|", "|en|", "|es|", "|ex-yu|", "|fi|", "|fr|", "|gr|", "|hr|", "|hu|", "|in|", "|ir|", "|it|", "|lt|", "|mk|",
+                     "|mx|", "|nl|", "|no|", "|pl|", "|pt|", "|ro|", "|rs|", "|ru|", "|se|", "|si|", "|sk|", "|tr|", "|uk|", "|us|", "|yu|",
+
+                     "(", ")", "[", "]", "u-", "3d", "-", "'", " S01 ", " S02 ", " S03 ", " S04 ", " S05 ", " S06 ", " S07 ", " S08 ", " S09 ", " S10 ", " S11 ", " S12 ", " S13 ", " S14 ",
+                     " S15 ", " S16 ", "E01", "E02", "E03", "E04", "E05", "E06", "E07", "E08", "E09", "E10", "E11", "E12", "E13", "E14",  "E15", "E16", "E17", "E18", "E19", "E20", "E21", "E22", "E23", "E24", "E25", ]
+        for j in range (1900, 2025):
+            rep_name.append(str(j))
+
+        for i in rep_name:
+            name = name.replace(i, '')
+        if service or info:
+            evntN = re.sub("([\(\[]).*?([\)\]])|(: odc.\d+)|(\d+: odc.\d+)|(\d+ odc.\d+)|(:)|( -(.*?).*)|(,)", "", str(name))
+            self.evntNm = evntN
+            self.pstrNm = "/tmp/MMPoster/{}.jpg".format(self.evntNm)
+            if fileExists(self.pstrNm):
+                size = self.instance.size()
+                self.picload = ePicLoad()
+                sc = AVSwitch().getFramebufferScale()
+                if self.picload:
+                    self.picload.setPara((size.width(), size.height(), sc[0], sc[1], False, 1, '#00000000'))
+                result = self.picload.startDecode(self.pstrNm, 0, 0, False)
+                if result == 0:
+                    ptr = self.picload.getData()
+                    if ptr != None:
+                        self.instance.setPixmap(ptr)
+                        self.instance.show()
+                else:
+                    self.instance.hide()
+
+    def savePoster(self):
+        if not os.path.isdir(folder_path):
+            os.makedirs(folder_path)
+        try:
+            pic_image = urlopen(self.url_poster)
+            respimage = pic_image.read()
+            image = open(self.pstrNm, "wb")
+            image.write(respimage)
+            image.close()
+            self.showPoster()
+        except:
+            pass
