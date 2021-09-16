0e65797da4dcc1776c5e7eb4a27dd0069aa1e346
madhouse <35741027+m4dhouse@users.noreply.github.com>
2021-09-16 23:58:09 +0200

    Add infobar poster

--- egami-common/usr/lib/enigma2/python/Components/Renderer/ODposter.py
+++ egami-common/usr/lib/enigma2/python/Components/Renderer/ODposter.py
@@ -5,13 +5,18 @@ from enigma import ePixmap, ePicLoad, eTimer
 from Components.AVSwitch import AVSwitch
 try:
     from urllib.request import urlopen, quote
+    from _thread import start_new_thread
 except ImportError:
     from urllib2 import urlopen, quote
+    from thread import start_new_thread
 import json
 import re
 from os import path, walk, popen, makedirs
 import socket
 from shutil import rmtree
+from Components.config import config
+leng = config.osd.language.value
+leng = leng.replace('_', '-')
 
 # Dir Poster Download
 folder_path = "/tmp/odposter/"
@@ -20,26 +25,25 @@ apikey = "f23c2bd91113daf777dcba03990aea77"
 try:
     folder_size=sum([sum(map(lambda fname: path.getsize(path.join(folder_path, fname)), files)) for folder_path, folders, files in walk(folder_path)])
     odposter = "%0.f" % (folder_size/(1024*1024.0))
-    if odposter >= "5": # Elimino al raggiungimento di 5mb
+    if odposter >= "5":
         rmtree(folder_path)
 except:
     pass
 
-# Recupero lingua sistema
-leng = popen("cat /etc/enigma2/settings | grep config.osd.language|sed '/^config.osd.language=/!d'").read().replace('config.osd.language=', '').replace('_', '-').replace('\n', '')
-
 class ODposter(Renderer):
     def __init__(self):
         Renderer.__init__(self)
-        self.timer10 = eTimer()
-    # Verifico presenza internet
+        self.intCheck()
+        self.timer = eTimer()
+        self.timer.callback.append(self.showPoster)
+
     def intCheck(self):
         try:
-            socket.setdefaulttimeout(0.5)
+            socket.setdefaulttimeout(1)
             socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(("8.8.8.8", 53))
             return True
         except:
-            return False
+            return
 
     GUI_WIDGET = ePixmap
     def changed(self, what):
@@ -48,104 +52,72 @@ class ODposter(Renderer):
         if what[0] == self.CHANGED_CLEAR:
             self.instance.hide()
         if what[0] != self.CHANGED_CLEAR:
-            self.delay()
-
-    def info(self):
-        if self.downloading:
-            return
-        self.downloading = True
-        self.event = self.source.event
-        if self.event:
-            evnts = self.event.getEventName()
-            evnt = evnts.replace('FILM ', '').replace('FILM - ', '').replace('film - ', '').replace('TELEFILM ', '').replace('TELEFILM - ', '').replace('telefilm - ', '')
-            evntN = re.sub("([\(\[]).*?([\)\]])|(: odc.\d+)|(\d+: odc.\d+)|(\d+ odc.\d+)|(:)|( -(.*?).*)|(,)|!", "", evnt).rstrip()
-            self.evntNm = evntN
-            self.pstrNm = "/tmp/odposter/{}.jpg".format(self.evntNm)
-            if self.intCheck() == True:
-                try:
-                    url = 'http://api.themoviedb.org/3/search/tv?api_key={}&query={}'.format(apikey,quote(self.evntNm))
-                    url2 = urlopen(url).read().decode('utf-8')
-                    jurl = json.loads(url2)
-                    if 'results' in jurl:
-                        if 'id' in jurl['results'][0]:
-                            ids = jurl['results'][0]['id']
-                    url_2 = 'http://api.themoviedb.org/3/tv/' + str(ids) + '?api_key={}&language={}'.format(apikey,str(leng))
-                    url_3 = urlopen(url_2).read().decode('utf-8')
-                    data2 = json.loads(url_3)
-                    poster = data2['poster_path']
-                    if poster:
-                        self.url_poster = "http://image.tmdb.org/t/p/w185{}".format(poster) #w185 risoluzione poster
-                        self.savePoster()
-                    self.timer10.stop()
-                except:
-                    try:
-                        url = 'http://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(apikey,quote(self.evntNm))
-                        url2 = urlopen(url).read().decode('utf-8')
-                        jurl = json.loads(url2)
-                        if 'results' in jurl:
-                            if 'id' in jurl['results'][0]:
-                                ids = jurl['results'][0]['id']
-                        url_2 = 'http://api.themoviedb.org/3/movie/' + str(ids) + '?api_key={}&language={}'.format(apikey,str(leng))
-                        url_3 = urlopen(url_2).read().decode('utf-8')
-                        data2 = json.loads(url_3)
-                        poster = data2['poster_path']
-                        if poster:
-                            self.url_poster = "http://image.tmdb.org/t/p/w185{}".format(poster) #w185 risoluzione poster
-                            self.savePoster()
-                        self.timer10.stop()
-                    except:
-                        self.timer10.stop()
-                        pass
-        else:
-            self.instance.hide()
-            self.timer10.stop()
-
-    def delay(self):
-        self.downloading = False
-        self.event = self.source.event
-        if self.event:
-            evnts = self.event.getEventName()
-            evnt = evnts.replace('FILM ', '').replace('FILM - ', '').replace('film - ', '').replace('TELEFILM ', '').replace('TELEFILM - ', '').replace('telefilm - ', '')
-            evntN = re.sub("([\(\[]).*?([\)\]])|(: odc.\d+)|(\d+: odc.\d+)|(\d+ odc.\d+)|(:)|( -(.*?).*)|(,)|!", "", evnt).rstrip()
-            self.evntNm = evntN
-            self.pstrNm = "/tmp/odposter/{}.jpg".format(self.evntNm)
-            if path.exists(self.pstrNm):
-                self.showPoster()
-            else:
-                self.instance.hide()
-                try:
-                    self.timer10.callback.append(self.info)
-                except:
-                    self.timer10_conn = self.timer10.timeout.connect(self.info)
-                self.timer10.start(450, False) # Timer avvio Download Poster
-        else:
-            self.instance.hide()
+            self.timer.start(100, True)
 
     def showPoster(self):
-        if path.exists(self.pstrNm):
-            size = self.instance.size()
-            self.picload = ePicLoad()
-            sc = AVSwitch().getFramebufferScale()
-            if self.picload:
-                self.picload.setPara([size.width(), size.height(), sc[0], sc[1], False, 1, '#00000000'])
-                if path.exists('/var/lib/dpkg/status'):
-                    self.picload.startDecode(self.pstrNm, False)
-                else:
-                    self.picload.startDecode(self.pstrNm, 0, 0, False)
-            ptr = self.picload.getData()
-            if ptr != None:
-                self.instance.setPixmap(ptr)
-                self.instance.show()
-            else:
-                self.instance.hide()
-    # Download Poster
-    def savePoster(self):
-        if not path.exists(folder_path):
+        if not path.isdir(folder_path):
             makedirs(folder_path)
-        pic_image = urlopen(self.url_poster).read()
+        self.instance.hide()
+        self.event = self.source.event
+        if self.event is None:
+            self.instance.hide()
+            return
+        evnts = self.event.getEventName()
+        evntNm = re.sub("([\(\[]).*?([\)\]])|(: odc.\d+)|(\d+: odc.\d+)|(\d+ odc.\d+)|(:)|( -(.*?).*)|(,)|!|/.*|\|\s[0-9]+\+|[0-9]+\+|\s\d{4}\Z|([\(\[\|].*?[\)\]\|])|(\"|\"\.|\"\,|\.)\s.+|\"|:|Премьера\.\s|(х|Х|м|М|т|Т|д|Д)/ф\s|(х|Х|м|М|т|Т|д|Д)/с\s|\s(с|С)(езон|ерия|-н|-я)\s.+|\s\d{1,3}\s(ч|ч\.|с\.|с)\s.+|\.\s\d{1,3}\s(ч|ч\.|с\.|с)\s.+|\s(ч|ч\.|с\.|с)\s\d{1,3}.+|\d{1,3}(-я|-й|\sс-н).+|", "", evnts).rstrip()
+        evntNm = evntNm.replace('\xc2\x86', '').replace('\xc2\x87', '')
+        pstrNm = folder_path + "{}.jpg".format(evntNm)
+        if self.event:
+            if path.exists(pstrNm):
+                size = self.instance.size()
+                self.picload = ePicLoad()
+                sc = AVSwitch().getFramebufferScale()
+                if self.picload:
+                    self.picload.setPara([size.width(), size.height(), sc[0], sc[1], False, 1, '#00000000'])
+                    self.picload.startDecode(pstrNm, 0, 0, False)
+                ptr = self.picload.getData()
+                if ptr != None:
+                    self.instance.setPixmap(ptr)
+                    self.instance.show()
+                else:
+                    self.instance.hide()
+            else:
+                start_new_thread(self.info, ())
+        else:
+            self.instance.hide()
+            return
+
+    def info(self):
+        self.event = self.source.event
+        evnts = self.event.getEventName()
+        evntNm = re.sub("([\(\[]).*?([\)\]])|(: odc.\d+)|(\d+: odc.\d+)|(\d+ odc.\d+)|(:)|( -(.*?).*)|(,)|!|/.*|\|\s[0-9]+\+|[0-9]+\+|\s\d{4}\Z|([\(\[\|].*?[\)\]\|])|(\"|\"\.|\"\,|\.)\s.+|\"|:|Премьера\.\s|(х|Х|м|М|т|Т|д|Д)/ф\s|(х|Х|м|М|т|Т|д|Д)/с\s|\s(с|С)(езон|ерия|-н|-я)\s.+|\s\d{1,3}\s(ч|ч\.|с\.|с)\s.+|\.\s\d{1,3}\s(ч|ч\.|с\.|с)\s.+|\s(ч|ч\.|с\.|с)\s\d{1,3}.+|\d{1,3}(-я|-й|\sс-н).+|", "", evnts).rstrip()
+        evntNm = evntNm.replace('\xc2\x86', '').replace('\xc2\x87', '')
+        pic_image = folder_path + "{}.jpg".format(evntNm)
         try:
-            with open(self.pstrNm, "wb") as f:
-                f.write(pic_image)
-            self.showPoster()
+            url = 'http://api.themoviedb.org/3/search/tv?api_key={}&query={}'.format(apikey,quote(evntNm))
+            jurl = json.loads(urlopen(url).read().decode('utf-8'))
+            if 'results' in jurl:
+                if 'id' in jurl['results'][0]:
+                    ids = jurl['results'][0]['id']
+            url_2 = 'http://api.themoviedb.org/3/tv/' + str(ids) + '?api_key={}&language={}'.format(apikey,str(leng))
+            data2 = json.loads(urlopen(url_2).read().decode('utf-8'))
+            poster = data2['poster_path']
+            url_poster = "http://image.tmdb.org/t/p/w185{}".format(poster)
+            with open(pic_image,'wb') as f:
+                f.write(urlopen(url_poster).read())
+                self.timer.start(10, True)
         except:
-            pass
+            try:
+                url = 'http://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(apikey,quote(evntNm))
+                jurl = json.loads(urlopen(url).read().decode('utf-8'))
+                if 'results' in jurl:
+                    if 'id' in jurl['results'][0]:
+                        ids = jurl['results'][0]['id']
+                url_2 = 'http://api.themoviedb.org/3/movie/' + str(ids) + '?api_key={}&language={}'.format(apikey,str(leng))
+                data2 = json.loads(urlopen(url_2).read().decode('utf-8'))
+                poster = data2['poster_path']
+                url_poster = "http://image.tmdb.org/t/p/w185{}".format(poster)
+                with open(pic_image,'wb') as f:
+                    f.write(urlopen(url_poster).read())
+                    self.timer.start(10, True)
+            except:
+                pass
